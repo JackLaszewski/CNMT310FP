e897058df49fb87269dd85509aa06dc2b827ca07
Jack <jlasz809@gmail.com>
2024-11-06 09:12:54 -0600

    fixing some errors with the admin page

--- views/admin.php
+++ views/admin.php
@@ -8,8 +8,8 @@ if(!isset($_SESSION['user_role'])){
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $apikey = "";
-    $apihash = "";
+    $apikey = "api86";
+    $apihash = "fefgwrv";
     
     //Get user input from form
     $coursename = $_POST['coursename'];
@@ -66,28 +66,32 @@ print "<head>";
 print "<meta charset=\"UTF-8\">";
 print "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
 print "<title>Admin Page</title>";
-print "<link rel=\"stylesheet\" href=\"style.css\">";
+print "<link rel=\"stylesheet\" type=\"text/css\" href=\"CSS/style.css\">";
 print "</head>";
 print "<body>";
-print "<form method=\"POST\" action=\"admin.php\">";
+print "<form method=\"POST\" action=\"admin.php\"<br>";
 print "<label for=\"coursename\">Course name:</label>";
-print "<input type=\"text\" id=\"coursename\" name=\"coursename\" required>";
+print "<input type=\"text\" id=\"coursename\" name=\"coursename\" required><br>";
 print "<label for=\"coursecode\">Coursecode:</label>";
-print "<input type=\"text\" id=\"coursecode\" name=\"coursecode\" required>";
-print "<label for=\"coursenum\">Course Number</label>";
-print "<input type=\"text\" id=\"coursenum\" name=\"coursenum\" required>";
-print "<label for=\"coursecredits\">Number of Credits:</label>";
-print "<input type=\"text\" id=\"coursecredits\" name=\"coursecredits\" required>";
-print "<label for=\"coursedesc\">Course Description</label>";
-print "<input type=\"text\" id=\"coursedesc\" name=\"coursedesc\" required>";
-print "<label for=\"courseinstr\">Course Instructor</label>";
-print "<input type=\"text\" id=\"courseinstr\" name=\"courseinstr\" required>";
-print "<label for=\"meetingtimes\">Meeting Times</label>";
-print "<input type=\"text\" id=\"meetingtimes\" name=\"meetingtimes\" required>";
-print "<label for=\"maxenroll\">Max Enroll</label>";
-print "<input type=\"text\" id=\"maxenroll\" name=\"maxenroll\" required>";
+print "<input type=\"text\" id=\"coursecode\" name=\"coursecode\" required><br>";
+print "<label for=\"coursenum\">Course Number</label> ";
+print "<input type=\"text\" id=\"coursenum\" name=\"coursenum\" required><br>";
+print "<label for=\"coursecredits\">Number of Credits:</label> ";
+print "<input type=\"text\" id=\"coursecredits\" name=\"coursecredits\" required><br>";
+print "<label for=\"coursedesc\">Course Description</label> ";
+print "<input type=\"text\" id=\"coursedesc\" name=\"coursedesc\" required><br>";
+print "<label for=\"courseinstr\">Course Instructor</label> ";
+print "<input type=\"text\" id=\"courseinstr\" name=\"courseinstr\" required><br>";
+print "<label for=\"meetingtimes\">Meeting Times</label> ";
+print "<input type=\"text\" id=\"meetingtimes\" name=\"meetingtimes\" required><br>";
+print "<label for=\"maxenroll\">Max Enroll</label> ";
+print "<input type=\"text\" id=\"maxenroll\" name=\"maxenroll\" required><br>";
 print "<button type=\"submit\">Add Class</button>";
 print "</form>";
+
+if (!empty($error_message)) {
+    print "<p class='error-message'>\$error_message</p>";
+}
 print "</body>";
 print "</html>";
 
